64ef133bdbdb4c4ce0002df5ac248e18829a6983
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-08-26 13:11:35 +0700

    deck of cards

--- CollectionsMethods/src/dev/lpa/Main.java
+++ CollectionsMethods/src/dev/lpa/Main.java
@@ -1,7 +1,10 @@
 package dev.lpa;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
     }
 }
